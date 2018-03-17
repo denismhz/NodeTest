42b0824fb9b0d5d095370b67a4224f77dad6e7d8
denismhz <denismanherz@gmx.de>
2018-03-17 19:22:06 +0100

    added app2.js

--- DemoApp/app.js
+++ DemoApp/app.js
@@ -1,7 +1,21 @@
-var something = require("cat-me");
-var knock = require("knock-knock-jokes");
+var express = require("express");
+var app = express();
 
-console.log(knock());
+app.get("/", function(req, res){
+	res.send("Hi there!");
+});
 
-console.log(something());
-console.log("from app.js");
+app.get("/bye", function(req, res) {
+	res.send("Goodbye");
+});
+
+app.get("/Dog", function(req, res) {
+	console.log("requested /Dog");
+	res.send("MEOW!");
+});
+
+app.listen(3000, function(){
+	console.log("Demo app listening on port 3000!");
+});
+ 
+
