d52db5e437af3ddcfc685dc83bbf09b5018def18
denismhz <denis.manherz@gmx.de>
2018-03-18 19:23:48 +0100

    asd

--- EJSDemo/app.js
+++ EJSDemo/app.js
@@ -6,6 +6,11 @@ app.get("/", function(req, res){
 	//res.send("<h1>Welcome to the homepage</h1><h2>balakdfajlsk</h2>");
 });
 
+app.get('/fallinlovewith/:thing', function(req, res){
+	var thing = req.params.thing;
+	res.render("lopve.ejs", {thingVar: thing});
+});
+
 app.listen(3000, function(){
 	console.log("server started port 3000");
 });
