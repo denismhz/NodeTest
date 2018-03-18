e0195bbfabd8cbb2d01676949701f6fe31911c63
denismhz <denis.mm@gmx.de>
2018-03-18 19:11:17 +0100

    EJSDemo

---
+++ EJSDemo/app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+	res.render("dogs.ejs");
+	//res.send("<h1>Welcome to the homepage</h1><h2>balakdfajlsk</h2>");
+});
+
+app.listen(3000, function(){
+	console.log("server started port 3000");
+});
