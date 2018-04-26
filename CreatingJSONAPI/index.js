e46497ebe063e04bcb7a54573c0b7c35d7a70ffe
denismhz <denis.mm@gmx.de>
2018-04-26 14:36:36 +0200

    Todo List Front-End

--- CreatingJSONAPI/index.js
+++ CreatingJSONAPI/index.js
@@ -3,12 +3,15 @@ var express     = require("express"),
     todoRoutes  = require("./routes/todos"),
     bodyParser  = require("body-parser");
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
 
 app.use("/api/todos", todoRoutes);
 
 app.get("/", function(req,res){
-    res.json({message: "Hi from js object"});
+    res.sendFile("index.html");
 });
 
 app.listen(3000, function(){
