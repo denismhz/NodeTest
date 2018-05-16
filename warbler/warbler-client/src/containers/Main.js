99964891428e814e1a11195e6b2bb9c7111dfece
denismhz <denis.mm@gmx.de>
2018-05-16 19:56:39 +0200

    Warbler Front 1

---
+++ warbler/warbler-client/src/containers/Main.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {Switch, Route, withRouter, Redirect} from "react-router-dom";
+import {connect} from "react-redux";
+import Homepage from "../components/Homepage";
+import AuthForm from "../components/AuthForm";
+import {authUser} from "../store/actions/auth";
+
+const Main = props => {
+  const {authUser} = props;
+  return(
+    <div className="container">
+      <Switch>
+        <Route exact path="/" render={props => <Homepage {...props} />} />
+        <Route exact path="/signin" render={props => {
+          return(
+            <AuthForm errors={errors} onAuth={authUser} buttonText="Log in" heading="welcome back." {...props} />
+            )
+        }}/>
+        <Route exact path="/signup" render={props => {
+          return(
+            <AuthForm errors= {errors} signUp onAuth={authUser} buttonText="Sign Up" heading="Join warbler bra" {...props} />
+            )
+        }}/>
+      </Switch>
+    </div>
+    );
+};
+
+function mapStateToProps(state){
+  return {
+    currentUser: state.currentUser,
+    errors: state.errors
+  };
+}
+
+export default withRouter(connect(mapStateToProps, {authUser})(Main));
